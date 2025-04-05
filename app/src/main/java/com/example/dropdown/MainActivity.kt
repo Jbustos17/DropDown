a63b2febf87a2946e353b2f4ad844689f80a8d65
Jbustos17 <juandavidbustos04@gmail.com>
2025-04-04 22:57:36 -0500

    dropdown terminado

--- app/src/main/java/com/example/dropdown/MainActivity.kt
+++ app/src/main/java/com/example/dropdown/MainActivity.kt
@@ -1,18 +1,17 @@
 package com.example.dropdown
 
-import Dropdowns
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material3.*
-import com.example.dropdown.ui.theme.DropdownTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
-import com.example.dropdown.Screen.navigationExample
-import com.example.dropdown.ui.theme.DropdownTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
 
-            navigationExample()
+
 
         }
     }
